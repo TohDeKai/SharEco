7cc8a864939eb61e3a0696f363d3eaa5d4a2f6e1
Eden Yew <eden_yew@u.nus.edu>
2023-09-01 10:49:23 +0800

    server configuration

--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
 require("dotenv").config();
-const express = require("express")
+const express = require("express");
+const { Pool } = require("pg");
 
-const app = express()
+const app = express();
 
 // Choosing port for Express to listen on
+const port = process.env.PORT || 4000;
 
-const port = process.env.PORT;
+// PostgreSQL connection pool configuration
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+// Testing the database connection
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error("Error acquiring client:", err.stack);
+  }
+  console.log("Connected to PostgreSQL database");
+});
+
 app.listen(port, () => {
-    console.log(`Server is up and listening on Port ${port}`);
-})
+  console.log(`Server is up and listening on Port ${port}`);
+});
