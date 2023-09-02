390e1ba0d0fdb6ce585cdf06dc23e4c4c53961fa
Amirul Shyakir <amshyaswad@gmail.com>
2023-09-03 01:20:50 +0800

    set up folder directory

--- SharEco-mobile/app/index.js
+++ SharEco-mobile/app/index.js
@@ -1,4 +1,7 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+
+import { COLORS, icons, images, SIZES } from '../constants';
 
 const Home = () => {
   return (
