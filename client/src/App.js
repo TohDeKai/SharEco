e2e80dca631ba8f28b00eade79b09c6fec4d5734
TohDeKai <tohdekai@outlook.com>
2023-09-03 17:37:18 +0800

    make default route signin

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignIn from "./login";
 import Home from "./home";
 
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
         <Route path="signin" element={<SignIn />} />
         <Route path="home" element={<Home />} />
       </Routes>
