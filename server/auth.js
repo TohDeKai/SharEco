f71eebc4d44f53803c3547aea6797a07cce3f2bd
TohDeKai <tohdekai@outlook.com>
2023-09-07 15:28:17 +0800

    setup JWT token

--- server/auth.js
+++ server/auth.js
@@ -1,4 +1,5 @@
 const db = require("./queries");
+const jwt = require("jsonwebtoken");
 
 const SignIn = async (req, res) => {
   const { username, password } = req.body; // Destructure username and password from the request body
@@ -17,9 +18,18 @@ const SignIn = async (req, res) => {
     // Check if the provided password matches the user's password
     if (password === user.password) {
       // If the passwords match, send a success response
+      const jwtToken = jwt.sign(
+        {
+          id: user.id,
+          username: user.username,
+        },
+        process.env.JWT_SECRET
+      );
+
       res.status(200).json({
         status: "success",
         message: "Logged in successfully",
+        token: jwtToken,
       });
     } else {
       // If the passwords don't match, send a 400 response
