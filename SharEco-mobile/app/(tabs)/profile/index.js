69f1a2d5825e77f1869746292ca4840976a2d1e5
Amirul Shyakir <amshyaswad@gmail.com>
2023-09-07 14:01:28 +0800

    added profile page skeleton

--- SharEco-mobile/app/(tabs)/profile/index.js
+++ SharEco-mobile/app/(tabs)/profile/index.js
@@ -1,20 +1,85 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Dimensions } from 'react-native';
 import React from 'react';
 import { useAuth } from '../../../context/auth';
+
+//components
+import { Ionicons } from '@expo/vector-icons'; 
 import SafeAreaContainer from '../../../components/containers/SafeAreaContainer';
+import RegularText from '../../../components/text/RegularText';
 import { colours } from '../../../components/ColourPalette';
-const { primary, secondary, black } = colours;
+import UserAvatar from '../../../components/UserAvatar';
+const { primary, secondary, black, white } = colours;
+
+const viewportHeightInPixels = (percentage) => {
+  const screenHeight = Dimensions.get('window').height;
+  return percentage / 100 * screenHeight;
+}
+
+const ProfileHeader = () => {
+  return (
+    <View style={styles.header}>
+      <View style={styles.headerGreen}>
+        <Ionicons name="create-outline" color={white} size={26} style={styles.headerIcon} onPress={() => console.log('hello')}/>
+        <Ionicons name="settings-outline" color={white} size={26} style={styles.headerIcon} onPress={() => console.log('hello')}/>
+      </View>
+      <View style={styles.headerWhite}>
+        <RegularText>THIS IS THE SECOND PART OF HEADER</RegularText>
+      </View>
+      <UserAvatar size="small" source={require('../../../assets/icon.png')}/>
+    </View>
+  )
+}
 
 const profile = () => {
   const { signOut } = useAuth();
   return (
-    <SafeAreaContainer>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: secondary}}>
-        <Text>profile</Text>
-        <Text onPress={() => signOut()}>Sign Out</Text>
-      </View>
-    </SafeAreaContainer>
+    <ScrollView>
+      <ProfileHeader/>
+      <SafeAreaContainer>
+        <View style={styles.listingView}>
+          <Text>profile</Text>
+          <Text onPress={() => signOut()}>Sign Out</Text>
+        </View>
+      </SafeAreaContainer>
+    </ScrollView>
   )
 }
 
-export default profile
+export default profile;
+
+const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    height: viewportHeightInPixels(40),
+    borderColor: "red",
+    borderWidth: 1,
+  },
+  headerGreen: {
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingHorizontal: 25,
+    backgroundColor: secondary,
+  },
+  headerIcon: {
+    marginLeft: 5,
+  },
+  avatar: {
+    position: 'relative',
+    left: 25,
+    top: viewportHeightInPixels(20),
+  },
+  headerWhite: {
+    flex: 0.5,
+    paddingHorizontal: 25,
+    backgroundColor: white,
+  },
+  listingView: {
+    flex: 1,
+    minHeight: viewportHeightInPixels(65),
+    backgroundColor: white,
+    justifyContent: 'center', 
+    alignItems: 'center',
+  }
+})
