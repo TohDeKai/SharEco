67f6ff31f826b7915eeb4642cbeb90ffeb3088a9
TohDeKai <tohdekai@outlook.com>
2023-09-11 16:00:50 +0800

    display user username

--- admin/src/home.js
+++ admin/src/home.js
@@ -2,19 +2,24 @@ import React from "react";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import { useSignOut } from "react-auth-kit";
+import { useAuthUser } from "react-auth-kit";
 
 const Home = () => {
   const navigate = useNavigate();
   const signOut = useSignOut();
+  const auth = useAuthUser();
 
   const handleLogout = () => {
     signOut();
     navigate("../signin");
   };
 
+  console.log("user: ", auth().user);
+
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>Welcome {auth().username}</h1>
+
       <Button
         variant="contained"
         color="primary"
