1a9cace6c41296ebc26e0cdf15a115a4c33f09f9
Amirul Shyakir <amshyaswad@gmail.com>
2023-09-19 23:25:13 +0800

    set up environment variables for BASE_URL

--- SharEco-mobile/app/(auth)/sign-in.js
+++ SharEco-mobile/app/(auth)/sign-in.js
@@ -2,9 +2,6 @@ import {
   Text,
   View,
   Image,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  ScrollView,
   StyleSheet,
   Dimensions,
 } from "react-native";
@@ -12,9 +9,9 @@ import React, { useState } from "react";
 import { Formik } from "formik";
 import axios from "axios";
 import { useAuth } from "../../context/auth";
-import SafeAreaContainer from "../../components/containers/SafeAreaContainer";
 
 //components
+import SafeAreaContainer from "../../components/containers/SafeAreaContainer";
 import StyledTextInput from "../../components/inputs/LoginTextInputs";
 import RoundedButton from "../../components/buttons/RoundedButton";
 import MessageBox from "../../components/text/MessageBox";
@@ -22,6 +19,8 @@ import { Link, router } from "expo-router";
 import RegularText from "../../components/text/RegularText";
 import { colours } from "../../components/ColourPalette";
 const { primary, white } = colours;
+const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
+
 const viewportHeightInPixels = (percentage) => {
   const screenHeight = Dimensions.get("window").height;
   return (percentage / 100) * screenHeight;
@@ -39,7 +38,7 @@ export default function SignIn() {
 
     try {
       const response = await axios.post(
-        `http://192.168.2.90:4000/api/v1/user/signIn`,
+        `http://${BASE_URL}:4000/api/v1/user/signIn`,
         {
           username,
           password,
@@ -48,7 +47,7 @@ export default function SignIn() {
       console.log(response.status);
       if (response.status == 200) {
         const userDataResponse = await axios.get(
-          `http://192.168.2.90:4000/api/v1/users/username/${username}`
+          `http://${BASE_URL}:4000/api/v1/users/username/${username}`
         );
         if (userDataResponse.status === 200) {
           // Successfully retrieved user data, useAuth to signIn with this user
