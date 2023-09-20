cd26cf8b71524e0685fb84567a87317e9a9536a1
TohDeKai <tohdekai@outlook.com>
2023-09-21 01:10:30 +0800

    add create new admin button w/o functionality

--- admin/src/home.js
+++ admin/src/home.js
@@ -1,8 +1,38 @@
 import React from "react";
+import Button from "@mui/material/Button";
 import Sidebar from "./sidebar";
 import { styles } from "./styles";
 import { ThemeProvider } from "@mui/material/styles";
+import {
+  Box,
+  Toolbar,
+  Typography,
+  TextField,
+  FormControl,
+  InputLabel,
+  Input,
+  InputAdornment,
+  FilledInput,
+} from "@mui/material";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
 const Home = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <ThemeProvider theme={styles.shareCoTheme}>
       <div style={{ display: "flex" }}>
@@ -10,6 +40,60 @@ const Home = () => {
         <Sidebar />
 
         {/* Content */}
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+        >
+          <h1>Dashboard</h1>
+
+          {/* Create New Admin Button */}
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleClickOpen}
+            size="medium"
+          >
+            Create New Admin
+          </Button>
+
+          {/* Create New Admin Dialog */}
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Creating new admin account</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                You are now creating a new account that will be used to access
+                this admin web portal.
+              </DialogContentText>
+
+              <TextField
+                sx={{ mt: 2 }}
+                autoFocus
+                margin="dense"
+                id="username"
+                label="Username"
+                type="username"
+                fullWidth
+                variant="outlined"
+              />
+              <TextField
+                sx={{ mb: 1 }}
+                autoFocus
+                margin="dense"
+                id="password"
+                label="Password"
+                type="password"
+                fullWidth
+                variant="outlined"
+              />
+            </DialogContent>
+            <DialogActions sx={{ mb: 1 }}>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button variant="contained" onClick={handleClose}>
+                Register New Account
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Box>
       </div>
     </ThemeProvider>
   );
