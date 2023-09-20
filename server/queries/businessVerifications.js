7268db7119c79a400739dab2486dee021dcdd1a9
TohDeKai <tohdekai@outlook.com>
2023-09-20 23:31:20 +0800

    business verification endpoint CRUD functionality

---
+++ server/queries/businessVerifications.js
@@ -0,0 +1,88 @@
+const Pool = require("pg").Pool;
+
+// PostgreSQL connection pool configuration
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+// Get all business verifications
+const getBusinessVerifications = async () => {
+  try {
+    const result = await pool.query(
+      'SELECT * FROM "sharEco-schema"."businessVerification"'
+    );
+    return result.rows;
+  } catch (err) {
+    throw err;
+  }
+};
+
+// Create new business verification
+const createBusinessVerification = async (
+  UEN,
+  documents,
+  approved,
+  originalUserId
+) => {
+  try {
+    const result = await pool.query(
+      `INSERT INTO "sharEco-schema"."businessVerification" 
+        ("UEN", documents, approved, "originalUserId") values ($1, $2, $3, $4) returning *`,
+      [UEN, documents, approved, originalUserId]
+    );
+    return result.rows[0];
+  } catch (err) {
+    throw err;
+  }
+};
+
+// Update business verification
+const updateBusinessVerification = async (
+  businessVerificationId,
+  UEN,
+  documents,
+  approved,
+  originalUserId
+) => {
+  try {
+    const result = await pool.query(
+      `UPDATE "sharEco-schema"."businessVerification" 
+        SET "UEN" = $1, 
+        documents = $2,
+        approved = $3,
+        "originalUserId" = $4
+        WHERE "businessVerificationId" = $5
+        RETURNING *`,
+      [UEN, documents, approved, originalUserId, businessVerificationId]
+    );
+    return result.rows[0];
+  } catch (err) {
+    throw err;
+  }
+};
+
+// Delete business verification
+const deleteBusinessVerification = async (businessVerificationId) => {
+  try {
+    const result = await pool.query(
+      `DELETE FROM "sharEco-schema"."businessVerification" 
+        WHERE "businessVerificationId" = $1
+        RETURNING *`,
+      [businessVerificationId]
+    );
+    return result.rows[0];
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  getBusinessVerifications,
+  createBusinessVerification,
+  updateBusinessVerification,
+  deleteBusinessVerification,
+};
