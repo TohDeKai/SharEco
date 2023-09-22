d6866697109195d462fc22736ec910448c0fa8a7
leeannong <leeannonggg@gmail.com>
2023-09-22 15:18:42 +0800

    Construction message for empty pages

--- SharEco-mobile/app/(tabs)/home/index.js
+++ SharEco-mobile/app/(tabs)/home/index.js
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
 import SafeAreaContainer from "../../../components/containers/SafeAreaContainer";
 import { colours } from "../../../components/ColourPalette";
 import RegularText from "../../../components/text/RegularText";
-const { white } = colours;
+const { white, primary } = colours;
 
 const home = () => {
   return (
@@ -18,10 +20,40 @@ const home = () => {
           marginHorizontal: "10%",
         }}
       >
-        <RegularText typography="H1">Browsing Feature Coming Soon!</RegularText>
-        <RegularText typography="B2">
-          We will be launching this feature on 8 October 2023, 
-          mark your calendars!
+        <Ionicons
+          name="construct"
+          color={primary}
+          size={30}
+          style={{ marginBottom: 20 }}
+        />
+
+        <RegularText
+          typography="H1"
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Browsing Feature
+        </RegularText>
+        <RegularText
+          typography="H1"
+          style={{
+            textAlign: "center",
+            marginBottom: 20,
+          }}
+        >
+          Coming Soon
+        </RegularText>
+
+        <RegularText
+          typography="B2"
+          color=""
+          style={{
+            textAlign: "center",
+          }}
+        >
+          We will be launching this feature on 8 October 2023, mark your
+          calendars!
         </RegularText>
       </View>
     </SafeAreaContainer>
