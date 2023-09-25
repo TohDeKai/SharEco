0b65932b7225833db4e226c81fcf9714c0da1271
Amirul Shyakir <amshyaswad@gmail.com>
2023-09-25 23:56:50 +0800

    initialised homepage UI with searchbarheader and tabs

---
+++ SharEco-mobile/components/SearchBarHeader.js
@@ -0,0 +1,78 @@
+import { View, StyleSheet, Dimensions, Pressable } from 'react-native';
+import React from 'react';
+import { Ionicons } from "@expo/vector-icons";
+
+import StyledTextInput from './inputs/LoginTextInputs';
+import RegularText from "./text/RegularText";
+import { colours } from "./ColourPalette";
+const { black } = colours;
+
+const viewportHeightInPixels = (percentage) => {
+  const screenHeight = Dimensions.get("window").height;
+  return (percentage / 100) * screenHeight;
+};
+
+const viewportWidthInPixels = (percentage) => {
+  const screenWidth = Dimensions.get("window").width;
+  return (percentage / 100) * screenWidth;
+};
+
+const SearchBarHeader = (props) => {
+  return (
+    <View style={styles.headerContainer}>
+      <Pressable 
+            onPress={props.onPressMenu}
+            style={({ pressed }) => ({
+              opacity: pressed ? 0.5 : 1,
+              ...styles.icon 
+    })}>  
+        <Ionicons name={"menu-outline"} size={28} color={black}/>
+      </Pressable>
+
+      <StyledTextInput
+        placeholder="Search SharEco"
+        style={styles.searchBar}
+        isSearchBar={true}
+      />
+
+      <Pressable 
+            onPress={props.onPressWishlist}
+            style={({ pressed }) => ({
+              opacity: pressed ? 0.5 : 1,
+              ...styles.icon 
+    })}>  
+        <Ionicons name={"heart-outline"} size={28} color={black}/>
+      </Pressable>
+      <Pressable 
+            onPress={props.onPressChat}
+            style={({ pressed }) => ({
+              opacity: pressed ? 0.5 : 1,
+              color: pressed ? "red" : {black},
+              ...styles.icon 
+    })}>  
+        <Ionicons name={"chatbubble-outline"} size={28} color={black}/>
+      </Pressable>
+    </View>
+  );
+};
+
+export default SearchBarHeader;
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: '90%',
+    alignSelf: "center",
+    gap: 10,
+  },
+  icon: {
+    top: 5,
+    flex: 1,
+  },
+  searchBar: {
+    minWidth: '65%',
+    marginTop: 0,
+  }
+});
