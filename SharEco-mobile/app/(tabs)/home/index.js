1e11c6a556e64ba511211f45474f11fa27bf25e3
leeannong <leeannonggg@gmail.com>
2023-09-26 16:37:49 +0800

    individual listing page (for others)

--- SharEco-mobile/app/(tabs)/home/index.js
+++ SharEco-mobile/app/(tabs)/home/index.js
@@ -1,14 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
 import SafeAreaContainer from "../../../components/containers/SafeAreaContainer";
 import { colours } from "../../../components/ColourPalette";
 import RegularText from "../../../components/text/RegularText";
+import { PrimaryButton } from "../../../components/buttons/RegularButton";
 const { white, primary } = colours;
 
 const home = () => {
+  const toIndivListing = () => {
+    router.push({pathname: "home/indivListing", params: {itemId: 137}});
+  }
   return (
     <SafeAreaContainer>
       <View
@@ -20,41 +24,9 @@ const home = () => {
           marginHorizontal: "10%",
         }}
       >
-        <Ionicons
-          name="construct"
-          color={primary}
-          size={30}
-          style={{ marginBottom: 20 }}
-        />
-
-        <RegularText
-          typography="H1"
-          style={{
-            textAlign: "center",
-          }}
-        >
-          Browsing Feature
-        </RegularText>
-        <RegularText
-          typography="H1"
-          style={{
-            textAlign: "center",
-            marginBottom: 20,
-          }}
-        >
-          Coming Soon
-        </RegularText>
-
-        <RegularText
-          typography="B2"
-          color=""
-          style={{
-            textAlign: "center",
-          }}
-        >
-          We will be launching this feature on 8 October 2023, mark your
-          calendars!
-        </RegularText>
+        <PrimaryButton onPress={toIndivListing}>
+          Individual Listing
+        </PrimaryButton>
       </View>
     </SafeAreaContainer>
   );
