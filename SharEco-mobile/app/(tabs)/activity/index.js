5722c8bbaa6e72e83bfc6906fabfef679ed354b3
mishellk <100346832+mishellk@users.noreply.github.com>
2023-09-28 14:08:09 +0800

    hader & tabs

--- SharEco-mobile/app/(tabs)/activity/index.js
+++ SharEco-mobile/app/(tabs)/activity/index.js
@@ -1,62 +1,264 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, StyleSheet, Pressable, ScrollView } from 'react-native';
+import React, { useState } from 'react';
 import { Ionicons } from "@expo/vector-icons";
 
 import SafeAreaContainer from '../../../components/containers/SafeAreaContainer';
 import { colours } from "../../../components/ColourPalette";
 import RegularText from '../../../components/text/RegularText';
-const { white, primary } = colours;
+const { black, inputbackground, white, primary, dark } = colours;
+
+const ActivityHeader  = () => {
+  const toWishlist = () => {
+    //push wishlist
+  }
+
+  const toChat = () => {
+    //push chat
+  }
+
+  return (
+    <View style={styles.header}>
+      <RegularText typography="H2">
+        Activity
+      </RegularText>
+
+      <View style={styles.icons}>
+        <Pressable
+          onPress={toWishlist}
+          style={({ pressed }) => ({
+            opacity: pressed ? 0.5 : 1,
+          })}
+        >
+          <Ionicons 
+            name="heart-outline"
+            color={black}
+            size={28}
+          />
+        </Pressable>
+
+        <Pressable
+          onPress={toChat}
+          style={({ pressed }) => ({
+            opacity: pressed ? 0.5 : 1,
+          })}
+        >
+          <Ionicons 
+            name="chatbubble-outline"
+            color={black}
+            size={26}
+          />
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const Tabs = ({ activeTab, handleTabPress }) => {
+  return (
+    <View style={styles.tabContainer}>
+      <Pressable
+        onPress={() => handleTabPress("Lending")}
+        style={({ pressed }) => [
+          { opacity: pressed ? 0.5 : 1 },
+          styles.tab,
+          activeTab === "Lending" && styles.activeTab,
+        ]}
+      >
+        <RegularText
+          typography="H4"
+          color={activeTab === "Lending" ? primary : dark}
+        >
+          Lending
+        </RegularText>
+      </Pressable>
+      <Pressable
+        onPress={() => handleTabPress("Borrowing")}
+        style={({ pressed }) => [
+          { opacity: pressed ? 0.5 : 1 },
+          styles.tab,
+          activeTab === "Borrowing" && styles.activeTab,
+        ]}
+      >
+        <RegularText
+          typography="H4"
+          color={activeTab === "Borrowing" ? primary : dark}
+        >
+          Borrowing
+        </RegularText>
+      </Pressable>
+      <Pressable
+        onPress={() => handleTabPress("Others")}
+        style={({ pressed }) => [
+          { opacity: pressed ? 0.5 : 1 },
+          styles.tab,
+          activeTab === "Others" && styles.activeTab,
+        ]}
+      >
+        <RegularText
+          typography="H4"
+          color={activeTab === "Others" ? primary : dark}
+        >
+          Others
+        </RegularText>
+      </Pressable>
+    </View>
+  );
+};
+
+const Pills = ({ pillItems, activeLendingPill, handlePillPress }) => {
+  return (
+    <View style={styles.pillContainer}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {pillItems.map((pill) => (
+          <Pressable
+            key={pill}
+            onPress={() => handlePillPress(pill)}
+            style={({ pressed }) => [
+              { opacity: pressed ? 0.5 : 1 },
+              styles.pill,
+              activeLendingPill === pill && styles.activePill,
+            ]}
+          >
+            <RegularText
+              typography="B1"
+              color={activeLendingPill === pill ? primary : dark}
+            >
+              {pill}
+            </RegularText>
+          </Pressable>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const Content = ({ activeTab }) => {
+  const [activeLendingPill, setActiveLendingPill] = useState("Upcoming");
+  const [activeBorrowingPill, setActiveBorrowingPill] = useState("Upcoming");
+
+  const lendingPill = ["Upcoming", "Ongoing", "Completed", "Cancelled"];
+  const borrowingPill = ["Upcoming", "Ongoing", "Pending", "Completed", "Cancelled"];
+  
+  // to include activeBorrowingPill
+  const handlePillPress = (pill) => {
+    setActiveLendingPill(pill);
+    console.log("Active pill: " + pill);
+  }
+  
+  return (
+    <View>
+      {activeTab == "Lending" && (
+        <View>
+          <View style={styles.rentalReqNotif}>
+            <Pressable>
+
+            </Pressable>
+
+            <Pressable>
+              
+            </Pressable>
+          </View>
+
+          <Pills 
+            pillItems={lendingPill} 
+            activeLendingPill={activeLendingPill} 
+            handlePillPress={handlePillPress}
+          />
+        </View>
+      )}
+
+      {(activeTab == "Borrowing" || activeTab == "Others") && (
+        <View 
+          style={{ height: "75%", justifyContent: "center", alignItems: "center" }}
+        >
+          <Ionicons
+            name="construct"
+            color={primary}
+            size={30}
+            style={{ marginBottom: 20, alignItems: "center" }}
+          />
+          <RegularText
+            typography="H3"
+            style={{ marginBottom: 5, textAlign: "center" }}
+          >
+            Under Construction
+          </RegularText>
+        </View>
+      )}
+    </View>
+  );
+};
 
 const activity = () => {
+  const [activeTab, setActiveTab] = useState("Lending");
+
+  const handleTabPress = (tabName) => {
+    setActiveTab(tabName);
+    console.log("Active tab: " + tabName);
+  };
+
   return (
     <SafeAreaContainer>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: white,
-          marginHorizontal: "10%",
-        }}
-      >
-        <Ionicons
-          name="construct"
-          color={primary}
-          size={30}
-          style={{ marginBottom: 20 }}
-        />
-
-        <RegularText
-          typography="H1"
-          style={{
-            textAlign: "center",
-          }}
-        >
-          Activity Feature
-        </RegularText>
-        <RegularText
-          typography="H1"
-          style={{
-            textAlign: "center",
-            marginBottom: 20,
-          }}
-        >
-          Coming Soon
-        </RegularText>
-
-        <RegularText
-          typography="B2"
-          color=""
-          style={{
-            textAlign: "center",
-          }}
-        >
-          We will be launching this feature on 8 October 2023, mark your
-          calendars!
-        </RegularText>
-      </View>
+      <ActivityHeader />
+      <Tabs activeTab={activeTab} handleTabPress={handleTabPress} />
+      <Content activeTab={activeTab} />
     </SafeAreaContainer>
   )
 }
 
 export default activity;
+
+const styles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    paddingTop: 40,
+    paddingBottom: 17,
+    paddingHorizontal: 25,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: inputbackground,
+    backgroundColor: white,
+    flexDirection: 'row',
+  },
+  icons: {
+    flexDirection: 'row',
+    alignItems: "center",
+    gap: 10,
+  },
+  tabContainer: {
+    flexDirection: "row",
+    width: "100%",
+  },
+  tab: {
+    flex: 1,
+    paddingVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: white,
+    borderBottomWidth: 2,
+    borderBottomColor: inputbackground,
+  },
+  activeTab: {
+    borderBottomColor: primary,
+  },
+  pillContainer: {
+    paddingHorizontal: 23,
+    paddingVertical: 18,
+  },
+  pill: {
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    borderRadius: 20,
+    backgroundColor: inputbackground,
+    marginRight: 10,
+  },
+  activePill: {
+    backgroundColor: white,
+    borderColor: primary,
+    borderWidth: 1,
+  },
+  rentalReqNotif: {
+
+  }
+})
---
+++ SharEco-mobile/components/containers/ActivityCard.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { View, StyleSheet, ScrollView } from 'react-native';
+
+import UserAvatar from "../UserAvatar";
+import RegularText from "../text/RegularText";
+import { colours } from "../ColourPalette";
+const { black, primary } = colours;
+
+const ActivityCard = ({ frequency, type, fulfillmentDate }) => {
+  <View>
+
+  </View>
+}
