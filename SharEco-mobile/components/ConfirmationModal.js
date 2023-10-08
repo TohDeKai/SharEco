4591c26be1c9d563acca02bdbb1791112757a38b
mishellk <100346832+mishellk@users.noreply.github.com>
2023-10-08 16:02:04 +0800

    merge with edenyew branch

--- SharEco-mobile/components/ConfirmationModal.js
+++ SharEco-mobile/components/ConfirmationModal.js
@@ -1,43 +1,293 @@
-import React from "react";
-import { StyleSheet, Modal, Text, View, Button } from "react-native";
+import React, { useState, useRef } from "react";
+import { StyleSheet, Modal, Text, View, Button, TextInput } from "react-native";
 import { PrimaryButton, SecondaryButton } from "./buttons/RegularButton";
 import { colours } from "./ColourPalette";
 import RegularText from "./text/RegularText";
 const { primary, white, black, secondary } = colours;
+import StyledTextInput from "../components/inputs/LoginTextInputs";
+import MessageBox from "../components/text/MessageBox";
+import { Formik } from "formik";
 
-const ConfirmationModal = ({ isVisible, onClose, onConfirm }) => {
+const ConfirmationModal = ({
+  isVisible,
+  onClose,
+  onConfirm,
+  type,
+  ...props
+}) => {
+  // const [message, setMessage] = useState("");
+  // const [isSuccessMessage, setIsSuccessMessage] = useState("false");
+
+  // const handleCancel = async (values) => {
+  //   props.forCancellationData(values.cancellationReason);
+  // };
+
+  const [cancellationReason, setCancellationReason] = useState();
+  // const cancellationReasonRef = useRef();
+
+  // const handleConfirm = () => {
+  //   const text = cancellationReasonRef.current.value;
+  //   console.log("HANDLE CONFIRM", text);
+  //   setCancellationReason(text);
+  //   props.forCancellationData(text);
+  //   onConfirm();
+  // };
+
+  const startDate = new Date(props.rental.startDate);
+  const formattedStartDate = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(startDate);
+
+  const endDate = new Date(props.rental.endDate);
+  const formattedEndDate = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(endDate);
+
   return (
-    <View style={[styles.centeredView]}>
-      <Modal visible={isVisible} animationType="slide" transparent={false}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <RegularText
-              typography="H3"
-              color={black}
-              style={styles.modalStyle}
+    <View>
+      {
+        type === "Cancel" && (
+          <View style={[styles.centeredView]}>
+            <Modal
+              visible={isVisible}
+              animationType="slide"
+              transparent={false}
             >
-              Are you sure you want to delete? This action cannot be reversed.
-            </RegularText>
-            <View style={styles.nav}>
-              <View style={styles.buttonContainer}>
-                <SecondaryButton
-                  typography="H3"
-                  color={primary}
-                  onPress={onConfirm}
-                >
-                  Confirm
-                </SecondaryButton>
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <RegularText
+                    typography="H4"
+                    color={black}
+                    style={styles.modalStyle}
+                  >
+                    Are you sure you want to cancel? This cannot be reversed.
+                  </RegularText>
+                  <TextInput
+                    style={styles.input}
+                    multiline={true}
+                    onChangeText={(text) => {
+                      setCancellationReason(text);
+                      props.forCancellationData(text);
+                    }}
+                    placeholder="Tell us your reason for cancellation"
+                  />
+                  <View style={styles.nav}>
+                    <View style={styles.buttonContainer}>
+                      <SecondaryButton
+                        typography="H3"
+                        color={primary}
+                        onPress={onConfirm}
+                      >
+                        Confirm
+                      </SecondaryButton>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                      <PrimaryButton
+                        typography="H3"
+                        color={white}
+                        onPress={onClose}
+                      >
+                        Cancel
+                      </PrimaryButton>
+                    </View>
+                  </View>
                 </View>
-                <View style={styles.buttonContainer}>
-                <PrimaryButton typography="H3" color={white} onPress={onClose}>
-                  Cancel
-                </PrimaryButton>
               </View>
-              
-            </View>
+            </Modal>
           </View>
+        )
+        // (
+        //   <Formik
+        //     initialValues={{
+        //       cancellationReason: "",
+        //     }}
+        //     onSubmit={(values, actions) => {
+        //       if (values.cancellationReason == "") {
+        //         setMessage("Please fill in all fields");
+        //         setIsSuccessMessage(false);
+        //       } else {
+        //         handleCancel(values);
+        //         onConfirm();
+        //         actions.resetForm();
+        //       }
+        //     }}
+        //   >
+        //     {({ handleChange, handleBlur, handleSubmit, values }) => (
+        //       <View style={[styles.centeredView]}>
+        //         <Modal
+        //           visible={isVisible}
+        //           animationType="slide"
+        //           transparent={false}
+        //         >
+        //           <View style={styles.centeredView}>
+        //             <View style={styles.modalView}>
+        //               <RegularText
+        //                 typography="H3"
+        //                 color={black}
+        //                 style={styles.modalStyle}
+        //               >
+        //                 Are you sure you want to cancel? This action cannot be
+        //                 reversed.
+        //               </RegularText>
+        //               <View style={{ display: "flex", width: "100px" }}>
+        //                 <StyledTextInput
+        //                   placeholder="Enter reason for cancellation"
+        //                   onChangeText={handleChange("cancellationReason")}
+        //                   value={values.cancellationReason}
+        //                   maxLength={100}
+        //                   multiline={true}
+        //                   scrollEnabled={false}
+        //                   minHeight={120}
+        //                 />
+        //               </View>
+
+        //               <MessageBox
+        //                 style={{ marginTop: 10 }}
+        //                 success={isSuccessMessage}
+        //               >
+        //                 {message || " "}
+        //               </MessageBox>
+        //               <View style={styles.nav}>
+        //                 <View style={styles.buttonContainer}>
+        //                   <SecondaryButton
+        //                     typography="H3"
+        //                     color={primary}
+        //                     onPress={handleSubmit}
+        //                   >
+        //                     Confirm
+        //                   </SecondaryButton>
+        //                 </View>
+        //                 <View style={styles.buttonContainer}>
+        //                   <PrimaryButton
+        //                     typography="H3"
+        //                     color={white}
+        //                     onPress={onClose}
+        //                   >
+        //                     Cancel
+        //                   </PrimaryButton>
+        //                 </View>
+        //               </View>
+        //             </View>
+        //           </View>
+        //         </Modal>
+        //       </View>
+        //     )}
+        //   </Formik>
+        // )
+      }
+      {type === "Accept" && (
+        <View style={[styles.centeredView]}>
+          <Modal visible={isVisible} animationType="slide" transparent={false}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <RegularText
+                  typography="H4"
+                  color={black}
+                  style={styles.modalStyle}
+                >
+                  <Text style={styles.heading}>Please confirm rental for:</Text>
+                  {"\n"}
+                  {"\n"}
+                  <Text style={styles.subHeading}>
+                    Start Date {"       "} {formattedStartDate}
+                  </Text>{" "}
+                  {"\n"}
+                  {"\n"}
+                  <Text style={styles.subHeading}>
+                    End Date {"          "} {formattedEndDate}
+                  </Text>
+                  {"\n"}
+                  {"\n"}
+                  <Text style={styles.subHeading}>
+                    Location {"          "} {props.rental.collectionLocation}
+                  </Text>
+                  {"\n"}
+                  {"\n"}
+                  <Text style={styles.subHeading}>
+                    Deposit Fee {"  "} {props.rental.depositFee}
+                  </Text>
+                  {"\n"}
+                  {"\n"}
+                  <Text style={styles.subHeading}>
+                    Rental Fee {"      "} {props.rental.rentalFee}
+                  </Text>
+                  {"\n"}
+                  {"\n"}
+                  <Text style={styles.subHeading}>
+                    Total Fee {"          "} {props.rental.totalFee}
+                  </Text>
+                  {"\n"}
+                </RegularText>
+                <View style={styles.nav}>
+                  <View style={styles.buttonContainer}>
+                    <SecondaryButton
+                      typography="H3"
+                      color={primary}
+                      onPress={onConfirm}
+                    >
+                      Confirm
+                    </SecondaryButton>
+                  </View>
+                  <View style={styles.buttonContainer}>
+                    <PrimaryButton
+                      typography="H3"
+                      color={white}
+                      onPress={onClose}
+                    >
+                      Cancel
+                    </PrimaryButton>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </Modal>
         </View>
-      </Modal>
+      )}
+      {type === "Reject" && (
+        <View style={[styles.centeredView]}>
+          <Modal visible={isVisible} animationType="slide" transparent={false}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <RegularText
+                  typography="H4"
+                  color={black}
+                  style={styles.modalStyle}
+                >
+                  Are you sure? This action cannot be reversed.
+                </RegularText>
+                <View style={styles.nav}>
+                  <View style={styles.buttonContainer}>
+                    <SecondaryButton
+                      typography="H3"
+                      color={primary}
+                      onPress={onConfirm}
+                    >
+                      Confirm
+                    </SecondaryButton>
+                  </View>
+                  <View style={styles.buttonContainer}>
+                    <PrimaryButton
+                      typography="H3"
+                      color={white}
+                      onPress={onClose}
+                    >
+                      Cancel
+                    </PrimaryButton>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </Modal>
+        </View>
+      )}
     </View>
   );
 };
@@ -45,6 +295,18 @@ const styles = StyleSheet.create({
   modalStyle: {
     backgroundColor: white,
     marginVertical: 10,
+    textAlign: "center",
+    textAlignVertical: "center",
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "left",
+  },
+  subHeading: {
+    fontSize: 15,
+    textAlign: "left",
+    color: colours.placeholder,
   },
   centeredView: {
     flex: 1,
@@ -68,18 +330,24 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   nav: {
-    height:70,
+    height: 70,
     borderColor: white,
     flexDirection: "row",
     borderTopWidth: 1,
     paddingHorizontal: 5,
-
   },
   buttonContainer: {
     flex: 0.5,
     paddingHorizontal: 5,
-    paddingVertical:0,
+    paddingVertical: 0,
     justifyContent: "center",
+  },
+  input: {
+    height: 55,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 280,
   },
 });
 export default ConfirmationModal;
--- SharEco-mobile/components/containers/RentalRequestCard.js
+++ SharEco-mobile/components/containers/RentalRequestCard.js
@@ -15,9 +15,11 @@ const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
 
 const RentalRequestCard = (props) => {
   const rental = props.newRentalRequest;
-  const [ isExpanded, setIsExpanded ] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
   const [item, setItem] = useState();
   const [user, setUser] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [modalType, setModalType] = useState("");
 
   const startDate = new Date(rental.startDate);
   const endDate = new Date(rental.endDate);
@@ -39,9 +41,7 @@ const RentalRequestCard = (props) => {
     month: "short",
     day: "numeric",
   });
-  const dailyRentalLength = Math.ceil(
-    dateDifferenceMs / (1000 * 60 * 60 * 24)
-  );
+  const dailyRentalLength = Math.ceil(dateDifferenceMs / (1000 * 60 * 60 * 24));
 
   // calculate hourly rental details
   const rentalDay = startDate.toLocaleDateString("en-US", {
@@ -74,8 +74,9 @@ const RentalRequestCard = (props) => {
   }
 
   const numOfDays = Math.floor(timeDifferenceMs / (1000 * 60 * 60 * 24));
-  const numOfHours = Math.floor((timeDifferenceMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  
+  const numOfHours = Math.floor(
+    (timeDifferenceMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
 
   useEffect(() => {
     async function fetchUserData() {
@@ -108,10 +109,11 @@ const RentalRequestCard = (props) => {
 
     fetchUserData();
     fetchItemData();
-  }, [rental.borrowerId, rental.status]);;
+  }, [rental.borrowerId, rental.status]);
 
-  const handleShowModal = () => {
+  const handleShowModal = (type) => {
     setShowModal(true);
+    setModalType(type);
   };
   const handleCloseModal = () => {
     setShowModal(false);
@@ -122,12 +124,10 @@ const RentalRequestCard = (props) => {
       let newStatus = "";
       const rentalId = id;
 
-      if (action === "Cancel") {
-        newStatus = "CANCELLED";
-      } else if (action === "Reject") {
+      if (action === "Reject") {
         newStatus = "REJECTED";
       } else if (action === "Accept") {
-        newStatus = "PENDING";
+        newStatus = "UPCOMING";
       }
 
       const response = await axios.patch(
@@ -143,22 +143,18 @@ const RentalRequestCard = (props) => {
 
   const toggleCollapse = () => {
     setIsExpanded(!isExpanded);
-  }
+  };
 
   return (
     <View style={styles.container}>
       <Pressable onPress={toggleCollapse}>
-        <View style={[
-          styles.collapsed,
-          !isExpanded && styles.collapsedBorder,
-        ]}>
+        <View style={[styles.collapsed, !isExpanded && styles.collapsedBorder]}>
           <View style={styles.rentalDetailsWithChevron}>
             <View style={styles.rentalDetails}>
               <Image
                 style={styles.image}
-                source={{ 
-                  uri: 
-                    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+                source={{
+                  uri: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
                 }}
               />
 
@@ -178,9 +174,7 @@ const RentalRequestCard = (props) => {
                   </RegularText>
                 )}
                 {!isExpanded && (
-                  <RegularText typography="B2">
-                    {rental.rentalFee}
-                  </RegularText>
+                  <RegularText typography="B2">{rental.rentalFee}</RegularText>
                 )}
               </View>
             </View>
@@ -188,14 +182,14 @@ const RentalRequestCard = (props) => {
             {isExpanded ? (
               <Ionicons
                 style={styles.chevron}
-                name='chevron-up'
+                name="chevron-up"
                 size={23}
                 color={black}
               />
             ) : (
               <Ionicons
                 style={styles.chevron}
-                name='chevron-down'
+                name="chevron-down"
                 size={23}
                 color={black}
               />
@@ -204,9 +198,7 @@ const RentalRequestCard = (props) => {
 
           {!isExpanded && (
             <View style={styles.countdown}>
-              <RegularText typography="Subtitle">
-                Accept in{' '}
-              </RegularText>
+              <RegularText typography="Subtitle">Accept in </RegularText>
               <RegularText typography="B3">
                 {numOfDays}D {numOfHours}H
               </RegularText>
@@ -214,7 +206,7 @@ const RentalRequestCard = (props) => {
           )}
         </View>
       </Pressable>
-        
+
       {isExpanded && (
         <View style={styles.expanded}>
           <Pressable 
@@ -238,25 +230,21 @@ const RentalRequestCard = (props) => {
           </Pressable>            
           
           <View style={styles.location}>
-            <RegularText typography="B3">
-              Location
-            </RegularText>
+            <RegularText typography="B3">Location</RegularText>
             <RegularText typography="Subtitle">
               {rental.collectionLocation}
             </RegularText>
           </View>
-          
+
           {rental.additionalRequest !== "" && (
             <View style={styles.additionalRequests}>
-              <RegularText typography="B3">
-                Additional Requests
-              </RegularText>
+              <RegularText typography="B3">Additional Requests</RegularText>
               <RegularText typography="Subtitle">
                 {rental.additionalRequest}
               </RegularText>
             </View>
           )}
-            
+
           <View style={styles.totalEarnings}>
             <RegularText typography="H3" color={dark}>
               Total Earnings
@@ -268,9 +256,7 @@ const RentalRequestCard = (props) => {
 
           <View style={styles.buttonContainerWithCountdown}>
             <View style={styles.countdown}>
-              <RegularText typography="Subtitle">
-                Accept in{' '}
-              </RegularText>
+              <RegularText typography="Subtitle">Accept in </RegularText>
               <RegularText typography="B3">
                 {numOfDays}D {numOfHours}H
               </RegularText>
@@ -285,22 +271,42 @@ const RentalRequestCard = (props) => {
                 />
               </Pressable>
               <View style={styles.button}>
-                <SecondaryButton typography="B3" color={placeholder}>
+                <SecondaryButton
+                  typography="B3"
+                  color={placeholder}
+                  onPress={() => handleShowModal("Reject")}
+                >
                   Reject
                 </SecondaryButton>
               </View>
               <View style={styles.button}>
-                <PrimaryButton typography="B3" color={white}>
+                <PrimaryButton
+                  typography="B3"
+                  color={white}
+                  onPress={() => handleShowModal("Accept")}
+                >
                   Accept
                 </PrimaryButton>
+                <ConfirmationModal
+                  isVisible={showModal}
+                  onConfirm={() =>
+                    handleStatus(
+                      modalType == "Accept" ? "Accept" : "Reject",
+                      rental.rentalId
+                    )
+                  }
+                  onClose={handleCloseModal}
+                  type={modalType}
+                  rental={rental}
+                />
               </View>
             </View>
           </View>
         </View>
       )}
     </View>
-  )
-}
+  );
+};
 
 export default RentalRequestCard;
 
@@ -312,7 +318,7 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     gap: 5,
   },
-  collapsedBorder:{
+  collapsedBorder: {
     borderBottomWidth: 2,
     borderBottomColor: inputbackground,
   },
@@ -336,7 +342,7 @@ const styles = StyleSheet.create({
   },
   countdown: {
     flexDirection: "row",
-    justifyContent: "flex-end"
+    justifyContent: "flex-end",
   },
   expanded: {
     gap: 10,
@@ -368,7 +374,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     paddingTop: 2,
-    gap: 2
+    gap: 2,
   },
   additionalRequests: {
     gap: 5,
@@ -388,5 +394,5 @@ const styles = StyleSheet.create({
   },
   button: {
     flex: 1,
-  }
-})
+  },
+});
