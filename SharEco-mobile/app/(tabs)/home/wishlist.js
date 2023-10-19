00928113bb4a87e9ad1a6535b124575af5165673
mishellk <100346832+mishellk@users.noreply.github.com>
2023-10-20 03:05:07 +0800

    wishlist done

--- SharEco-mobile/app/(tabs)/home/wishlist.js
+++ SharEco-mobile/app/(tabs)/home/wishlist.js
@@ -1,58 +1,108 @@
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
-import React from 'react';
-import { Ionicons } from "@expo/vector-icons";
+import { View, FlatList, RefreshControl, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../../context/auth';
+import { router } from 'expo-router';
 
 import SafeAreaContainer from '../../../components/containers/SafeAreaContainer';
 import RegularText from '../../../components/text/RegularText';
+import ListingCard from '../../../components/ListingCard';
 import { colours } from "../../../components/ColourPalette";
+import Header from '../../../components/Header';
+import axios from 'axios';
 const { white, primary } = colours;
+const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
 
 const wishlist = () => {
+  const [items, setItems] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [user, setUser] = useState("");
+  const { getUserData } = useAuth();
+
+  useEffect(() => {
+    async function fetchUserData() {
+      try {
+        const userData = await getUserData();
+        if (userData) {
+          setUser(userData);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchUserData();
+  }, []);
+
+  async function fetchWishlistItems() {
+    try {
+      console.log(user)
+      const response = await axios.get(
+        `http://${BASE_URL}:4000/api/v1/wishlist/userId/${user.userId}`
+      );
+
+      if (response.status === 200) {
+        console.log("Items fetched");
+        const wishlist = response.data.data.wishlist;
+        console.log(wishlist)
+        setItems(wishlist);
+      } else {
+        console.log("Failed to retrieve wishlist items");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchWishlistItems();
+  }, [user.userId])
+
+  useEffect(() => {
+    fetchWishlistItems();
+    setRefreshing(false);
+  }, [user.userId, refreshing])
+
+  const handleRefresh = async () => {
+    setRefreshing(true);
+  }
+
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
     <SafeAreaContainer>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: white,
-          marginHorizontal: "10%",
-        }}
-      >
-        <Ionicons
-          name="construct"
-          color={primary}
-          size={30}
-          style={{ marginBottom: 20 }}
-        />
-
-        <RegularText
-          typography="H1"
-          style={{
-            textAlign: "center",
-          }}
-        >
-          We are still working
-        </RegularText>
-        <RegularText
-          typography="H1"
-          style={{
-            textAlign: "center",
-            marginBottom: 20,
-          }}
-        >
-          on this feature
-        </RegularText>
-
-        <RegularText
-          typography="B2"
-          color=""
-          style={{
-            textAlign: "center",
-          }}
-        >
-          Stay tuned for updates on this feature!
-        </RegularText>
+      <Header title="Wishlist" action="back" onPress={handleBack} />
+      <View style={{ flex: 1, paddingHorizontal: "7%", marginTop: 20 }}>
+        {items.length > 0 ? (
+          <FlatList 
+            data={items}
+            numColumns={2}
+            scrollsToTop={false}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => 
+              <ListingCard 
+                item={item}
+                mine={false} 
+              />}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+            }
+          />
+        ) : (
+          <ScrollView 
+            style={{ marginTop: 160 }}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+            }
+          >
+            <RegularText
+              typography="H3"
+              style={{ textAlign: "center" }}
+            >
+              No items in your wishlist. 
+            </RegularText>
+          </ScrollView>
+        )}
       </View>
     </SafeAreaContainer>
   )
---
+++ server/queries/wishlist.js
@@ -0,0 +1,93 @@
+const Pool = require("pg").Pool;
+
+// PostgreSQL connection pool configuration
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+// Add to wishlist
+const createWishList = async (itemId, userId) => {
+  try {
+    const result = await pool.query(
+      `INSERT INTO "sharEco-schema"."wishlist" ("itemId", "userId") 
+      values ($1, $2) returning *`,
+      [itemId, userId]
+    );
+    return result.rows[0];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// Remove from wishlist
+const removeWishlist = async (itemId, userId) => {
+  try {
+    const result = await pool.query(
+      `DELETE FROM "sharEco-schema"."wishlist"
+      WHERE "itemId" = $1 AND "userId" = $2
+      RETURNING *`,
+      [itemId, userId]
+    );
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Get wishlist by itemId
+const getWishlistByItemId = async (itemId) => {
+  try {
+    const result = await pool.query(
+      `SELECT * FROM "sharEco-schema"."wishlist"
+      WHERE "itemId" = $1`,
+      [itemId]
+    );
+    return result.rows;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Get items by wishlist userId
+const getItemsByWishlistUserId = async (userId) => {
+  try {
+    const result = await pool.query(
+      `SELECT "item".*
+      FROM "sharEco-schema"."item" AS "item"
+      INNER JOIN "sharEco-schema"."wishlist" AS "wl" 
+      ON "item"."itemId" = "wl"."itemId"
+      WHERE "wl"."userId" = $1`,
+      [userId]
+    );
+    return result.rows;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Get wishlist by itemId and userId
+const getWishlistByItemIdAndUserId = async (itemId, userId) => {
+  try {
+    const result = await pool.query(
+      `SELECT * FROM "sharEco-schema"."wishlist"
+      WHERE "itemId" = $1 AND "userId" = $2`,
+      [itemId, userId]
+    );
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  createWishList,
+  removeWishlist,
+  getWishlistByItemId,
+  getItemsByWishlistUserId,
+  getWishlistByItemIdAndUserId,
+}
