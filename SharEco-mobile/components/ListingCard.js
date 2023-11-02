d4029d071caac37addf7c666b4e056eb8b3f3ab1
Shakstetic <amshyaswad@gmail.com>
2023-11-02 11:33:28 +0800

    added insights

--- SharEco-mobile/components/ListingCard.js
+++ SharEco-mobile/components/ListingCard.js
@@ -42,6 +42,10 @@ export default function ListingCard({ item, mine }) {
     router.push({ pathname: "profile/myListing", params: { itemId: itemId } });
   };
 
+  const toInsights = () => {
+    router.push({ pathname: "profile/insights", params: { itemId: itemId } });
+  };
+
   const toIndivListing = () => {
     router.push({pathname: "home/indivListing", params: { itemId: itemId }});
   }
@@ -115,6 +119,17 @@ export default function ListingCard({ item, mine }) {
                 </View>
               ))}
           </RegularText>
+          {mine && (
+            <Pressable onPress={toInsights}>
+              <View style={{flexDirection: "row", gap: "5px"}}>
+                <Ionicons name="trending-up-outline" size={18} color={secondary} />
+                <RegularText 
+                  typography="B3"
+                  color={secondary}>View Insights
+                </RegularText>
+              </View>
+            </Pressable>
+          )}
         </View>
       </View>
     </Pressable>
