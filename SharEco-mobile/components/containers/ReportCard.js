b08b6888361cd0d3b7d143c974e10a1cf07eeb77
TohDeKai <tohdekai@outlook.com>
2023-11-10 11:40:07 +0800

    FEAT: reportCard for DISPUTE 80% functionality, missing report reason + desc

--- SharEco-mobile/components/containers/ReportCard.js
+++ SharEco-mobile/components/containers/ReportCard.js
@@ -25,6 +25,17 @@ const ReportCard = ({ report }) => {
   const [reportedUser, setReportedUser] = useState({});
   const [item, setItem] = useState({});
   const [rental, setRental] = useState({});
+  const [isLending, setIsLending] = useState(false);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [isHourly, setIsHourly] = useState(false);
+  const [dateDifferenceMs, setDateDifferenceMs] = useState(0);
+  const [startDay, setStartDay] = useState("");
+  const [endDay, setEndDay] = useState("");
+  const [dailyRentalLength, setDailyRentalLength] = useState(0);
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [hourlyRentalLength, setHourlyRentalLength] = useState(0);
 
   useEffect(() => {
     async function fetchUserData(userId) {
@@ -55,11 +66,84 @@ const ReportCard = ({ report }) => {
       }
     }
 
+    async function fetchRentalData(rentalId) {
+      try {
+        const rentalResponse = await axios.get(
+          `http://${BASE_URL}:4000/api/v1/rentals/rentalId/${rentalId}`
+        );
+        if (rentalResponse.status === 200) {
+          const rentalData = rentalResponse.data.data.rental;
+          setRental(rentalData);
+          setStartDate(rentalData.startDate);
+          setEndDate(rentalData.endDate);
+          setIsHourly(rentalData.isHourly);
+          setDateDifferenceMs(rentalData.endDate - rentalData.startDate);
+          setStartDay(
+            new Date(rentalData.startDate).toLocaleDateString("en-US", {
+              month: "short",
+              day: "numeric",
+            })
+          );
+          setEndDay(
+            new Date(rentalData.endDate).toLocaleDateString("en-US", {
+              month: "short",
+              day: "numeric",
+            })
+          );
+          setDailyRentalLength(
+            Math.ceil(
+              (new Date(rentalData.endDate) - new Date(rentalData.startDate)) /
+                (1000 * 60 * 60 * 24)
+            )
+          );
+          setStartTime(
+            new Date(rentalData.startDate).toLocaleTimeString("en-US", {
+              hour: "numeric",
+            })
+          );
+          setEndTime(
+            new Date(rentalData.endDate).toLocaleTimeString("en-US", {
+              hour: "numeric",
+            })
+          );
+          setHourlyRentalLength(
+            Math.ceil(
+              (new Date(rentalData.endDate) - new Date(rentalData.startDate)) /
+                (1000 * 60 * 60)
+            )
+          );
+          const isLending =
+            rentalData.lenderId == report.reporterId ? true : false;
+          setIsLending[isLending];
+          fetchUserData(
+            isLending ? rentalData.borrowerId : rentalData.lenderId
+          );
+          fetchItemData(rentalData.itemId);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+
+    console.log(report.type);
     if (report.type == "LISTING") {
       fetchItemData(report.targetId);
+
+      console.log("Reported Item: " + item.itemId);
     }
     if (report.type == "USER") {
       fetchUserData(report.targetId);
+
+      console.log("Reported User: " + reportedUser.userId);
+    }
+    if (report.type == "DISPUTE") {
+      fetchRentalData(report.targetId);
+
+      console.log("Reported User: " + reportedUser.userId);
+      console.log("Reported Item: " + item.itemId);
+      console.log("Reported Rental: " + rental.rentalId);
+      console.log("Start Date: " + startDate);
+      console.log(isLending ? "Lender" : "Borrower");
     }
   }, []);
 
@@ -81,7 +165,35 @@ const ReportCard = ({ report }) => {
               {reportedUser.username}
             </RegularText>
           </View>
-        ) : null}
+        ) : (
+          <Pressable
+            style={({ pressed }) => ({
+              opacity: pressed ? 0.5 : 1,
+            })}
+            onPress={() =>
+              router.push({
+                pathname: "home/othersProfile",
+                params: { userId: reportedUser.userId },
+              })
+            }
+          >
+            <View style={styles.username}>
+              {reportedUser && (
+                <UserAvatar
+                  size="xsmall"
+                  source={{
+                    uri: `${AWS_GETFILE_URL}${reportedUser.userPhotoUrl}.jpeg`,
+                  }}
+                />
+              )}
+              {reportedUser && (
+                <RegularText typography="Subtitle">
+                  @{reportedUser.username}
+                </RegularText>
+              )}
+            </View>
+          </Pressable>
+        )}
 
         <View style={styles.countdown}>
           <RegularText typography="Subtitle">
@@ -127,7 +239,43 @@ const ReportCard = ({ report }) => {
                 </RegularText>
               </View>
             </View>
-          ) : null}
+          ) : (
+            <View style={styles.rentalDetails}>
+              <Image
+                style={styles.image}
+                source={{
+                  uri: item && item.images && item.images[0],
+                }}
+              />
+
+              {isHourly && (
+                <View style={styles.rentalDetailsText}>
+                  {item && (
+                    <RegularText typography="B2">{item.itemTitle}</RegularText>
+                  )}
+                  <RegularText typography="Subtitle">
+                    {startDay} - {endDay}
+                  </RegularText>
+                  <RegularText typography="Subtitle">
+                    {startTime} - {endTime} ({hourlyRentalLength}{" "}
+                    {hourlyRentalLength == 1 ? "Hour" : "Hours"})
+                  </RegularText>
+                </View>
+              )}
+
+              {!isHourly && (
+                <View style={styles.rentalDetailsText}>
+                  {item && (
+                    <RegularText typography="B2">{item.itemTitle}</RegularText>
+                  )}
+                  <RegularText typography="Subtitle">
+                    {startDay} - {endDay} ({dailyRentalLength}{" "}
+                    {dailyRentalLength == 1 ? "Day" : "Days"})
+                  </RegularText>
+                </View>
+              )}
+            </View>
+          )}
 
           <RegularText typography="B3" style={{ textAlign: "right" }}>
             {formatDate(report.reportDate)}
