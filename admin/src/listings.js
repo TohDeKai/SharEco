b59e5e3117df73e60acca4aba45da2bcdb7273f4
TohDeKai <tohdekai@outlook.com>
2023-11-15 00:37:00 +0800

    FEAT: show all LISTING reports

--- admin/src/listings.js
+++ admin/src/listings.js
@@ -34,7 +34,7 @@ import NorthOutlinedIcon from "@mui/icons-material/NorthOutlined";
 import SouthOutlinedIcon from "@mui/icons-material/SouthOutlined";
 import Chip from "@mui/material/Chip";
 
-const columns = [
+const listingColumns = [
   { id: "itemId", label: "Item ID", minWidth: 20 },
   { id: "itemTitle", label: "Item Title", minWidth: 50 },
   { id: "rentalRateHourly", label: "Hourly Rate", minWidth: 20 },
@@ -51,6 +51,21 @@ const columns = [
   },
 ];
 
+const reportColumn = [
+  { id: "reportId", label: "Report ID", minWidth: 50 },
+  { id: "reportDate", label: "Report Date", minWidth: 100 },
+  {
+    id: "reason",
+    label: "Reason",
+    minWidth: 170,
+  },
+  {
+    id: "description",
+    label: "Description",
+    minWidth: 170,
+  },
+];
+
 const Listing = ({}) => {
   // State for Disable Listing Snackbar
   const [disableSnackbarOpen, setDisableSnackbarOpen] = useState(false);
@@ -61,6 +76,7 @@ const Listing = ({}) => {
 
   // State for Enable Listing Snackbar
   const [enableSnackbarOpen, setEnableSnackbarOpen] = useState(false);
+  const [openReport, setReportOpen] = React.useState(false);
 
   const handleEnableSnackbarClose = () => {
     setEnableSnackbarOpen(false);
@@ -97,6 +113,7 @@ const Listing = ({}) => {
   const [rowsPerPage, setRowsPerPage] = React.useState(20);
   const [itemData, setItemData] = useState([]);
   const [showAllItems, setShowAllItems] = useState(false);
+  const [reportData, setReportData] = useState([]);
 
   const [selectedItemId, setSelectedItemId] = React.useState("");
   const [selectedUsername, setSelectedUsername] = React.useState("");
@@ -118,12 +135,23 @@ const Listing = ({}) => {
   const [selectedOtherLocation, setSelectedOtherLocation] = React.useState("");
   const [selectedCategory, setSelectedCategory] = React.useState("");
   const [selectedIsBusiness, setSelectedIsBusiness] = React.useState(false);
+  const [selectedItem, setSelectedItem] = React.useState({});
 
   const [loading, setLoading] = useState(false);
 
+  const [selectedReporterUsername, setSelectedReporterUsername] =
+    React.useState("");
+  const [selectedReportReason, setSelectedReportReason] = React.useState("");
+  const [selectedReportDescription, setSelectedReportDescription] =
+    React.useState("");
+  const [selectedReportedListingName, setSelectedReportedListingName] =
+    React.useState("");
+  const [selectedSupportingImages, setSelectedSupportingImages] =
+    React.useState([]);
+
   useEffect(() => {
     // Fetch item data when the component mounts
-    async function fetchData() {
+    async function fetchAllItemData() {
       try {
         const response = await axios.get("http://localhost:4000/api/v1/items");
         console.log(response);
@@ -133,7 +161,19 @@ const Listing = ({}) => {
         console.error("Error fetching item data:", error);
       }
     }
-    fetchData();
+    async function fetchAllItemReportData() {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/reports/type/LISTING"
+        );
+        const reports = response.data.data.report;
+        setReportData(reports);
+      } catch (error) {
+        console.error("Error fetching report data:", error);
+      }
+    }
+    fetchAllItemReportData();
+    fetchAllItemData();
   }, []);
 
   const handleChangePage = (event, newPage) => {
@@ -145,6 +185,43 @@ const Listing = ({}) => {
     setPage(0);
   };
 
+  const handleViewReport = async (reportId) => {
+    setLoading(true); // Set loading state to true
+    try {
+      const reportResponse = await axios.get(
+        `http://localhost:4000/api/v1/reports/reportId/${reportId}`
+      );
+
+      const report = reportResponse.data.data.report[0];
+
+      const itemResponse = await axios.get(
+        `http://localhost:4000/api/v1/items/itemId/${report.targetId}`
+      );
+
+      const item = itemResponse.data.data.item;
+
+      const reporterResponse = await axios.get(
+        `http://localhost:4000/api/v1/users/userId/${report.reporterId}`
+      );
+
+      const reporter = reporterResponse.data.data.user;
+
+      setSelectedReporterUsername(reporter.username);
+      setSelectedReportReason(report.reason);
+      setSelectedReportDescription(report.description);
+      setSelectedSupportingImages(report.supportingImages);
+      setSelectedReportedListingName(item.itemTitle);
+      setSelectedItem(item);
+      console.log("SUPPORTING IMAGES: " + report.supportingImages);
+      console.log(Array.isArray(selectedSupportingImages)); // Should log true if it's an array
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false); // Set loading state to false when the request is complete
+      setReportOpen(true);
+    }
+  };
+
   // To handle dialog
   const [openDisable, setDisableOpen] = React.useState(false);
   const [openEnable, setEnableOpen] = React.useState(false);
@@ -154,13 +231,25 @@ const Listing = ({}) => {
     setSelectedItemTitle(title);
     setSelectedItemId(itemId);
     setDisableOpen(true);
+    setReportOpen(false);
   };
 
   const handleEnableClickOpen = (title, itemId) => {
     setSelectedItemTitle(title);
     setSelectedItemId(itemId);
     setEnableOpen(true);
+    setReportOpen(false);
   };
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day.toString().padStart(2, "0")}/${month
+      .toString()
+      .padStart(2, "0")}/${year}`;
+  }
 
   const categories = [
     "Audio",
@@ -228,6 +317,7 @@ const Listing = ({}) => {
     setDisableOpen(false);
     setEnableOpen(false);
     setDetailsOpen(false);
+    setReportOpen(false);
   };
 
   const handleDisable = async () => {
@@ -319,6 +409,69 @@ const Listing = ({}) => {
             style={{ marginBottom: 30 }}
           />
 
+          {!showAllItems && (
+            <Paper sx={{ width: "100%", overflow: "hidden" }}>
+              <TableContainer sx={{ maxHeight: 440 }}>
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    <TableRow>
+                      {reportColumn.map((column) => (
+                        <TableCell
+                          key={column.id}
+                          align={column.align}
+                          style={{ minWidth: column.minWidth }}
+                        >
+                          {column.label}
+                        </TableCell>
+                      ))}
+                      <TableCell></TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {reportData
+                      .slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                      .map((row) => (
+                        <TableRow
+                          hover
+                          role="checkbox"
+                          tabIndex={-1}
+                          key={row.code}
+                        >
+                          {reportColumn.map((column) => (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.id === "reportDate"
+                                ? formatDate(row[column.id])
+                                : row[column.id]}
+                            </TableCell>
+                          ))}
+
+                          <TableCell>
+                            <Button
+                              variant="contained"
+                              onClick={() => handleViewReport(row.reportId)}
+                            >
+                              View Report
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={reportData.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </Paper>
+          )}
           {showAllItems && (
             <Paper sx={{ width: "100%", overflow: "hidden" }}>
               <Box sx={{ mb: 2 }}>
@@ -352,7 +505,7 @@ const Listing = ({}) => {
                 <Table stickyHeader aria-label="sticky table">
                   <TableHead>
                     <TableRow>
-                      {columns.map((column) => (
+                      {listingColumns.map((column) => (
                         <TableCell
                           key={column.id}
                           align={column.align}
@@ -426,7 +579,7 @@ const Listing = ({}) => {
                               )
                             }
                           >
-                            {columns.map((column) => {
+                            {listingColumns.map((column) => {
                               const value = row[column.id];
                               return (
                                 <TableCell key={column.id} align={column.align}>
@@ -607,6 +760,117 @@ const Listing = ({}) => {
               </TableBody>
             </Table>
           </DialogContent>
+          <DialogActions
+            style={{ justifyContent: "center", paddingTop: "20px" }}
+          >
+            {!selectedDisabled ? (
+              <Button
+                variant="contained"
+                onClick={() =>
+                  handleClickOpen(selectedItemId, selectedItemTitle)
+                }
+              >
+                Disable Item
+              </Button>
+            ) : (
+              <Button
+                variant="outlined"
+                onClick={() =>
+                  handleEnableClickOpen(selectedItemId, selectedItemTitle)
+                }
+              >
+                Enable Item
+              </Button>
+            )}
+          </DialogActions>
+        </Dialog>
+
+        {/* Dialog for Report Details */}
+        {/* Popup box to show all details of each listing */}
+        <Dialog
+          open={!loading && openReport}
+          onClose={handleClose}
+          scroll="paper"
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{`Reported Item: ${selectedReportedListingName}`}</DialogTitle>
+          <DialogContent>
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableCell style={cellStyle}>Reason</TableCell>
+                  <TableCell>{selectedReportReason}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell style={cellStyle}>Description</TableCell>
+                  <TableCell>
+                    {selectedReportDescription
+                      ? selectedReportDescription
+                      : "No Description Given"}
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell style={cellStyle}>Supporting Images</TableCell>
+                  <TableCell>
+                    {selectedSupportingImages.filter((imageUrl) => imageUrl)
+                      .length > 0
+                      ? selectedSupportingImages.map((imageUrl, index) =>
+                          imageUrl ? (
+                            <img
+                              key={index}
+                              src={imageUrl}
+                              alt={`Image ${index + 1}`}
+                              style={{
+                                maxWidth: "100%",
+                                maxHeight: "100%",
+                                marginRight: "5px",
+                              }}
+                            />
+                          ) : null
+                        )
+                      : "None"}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </DialogContent>
+          <DialogActions
+            style={{ justifyContent: "center", paddingTop: "20px" }}
+          >
+            <Button
+              variant="contained"
+              onClick={() =>
+                handleClickDetails(
+                  selectedItem.itemId,
+                  selectedItem.itemDescription,
+                  selectedItem.rentalRateHourly,
+                  selectedItem.rentalRateDaily,
+                  selectedItem.images,
+                  selectedItem.collectionLocations,
+                  selectedItem.userId,
+                  selectedItem.itemTitle,
+                  selectedItem.itemOriginalPrice,
+                  selectedItem.depositFee,
+                  selectedItem.disabled,
+                  selectedItem.otherLocation,
+                  selectedItem.category,
+                  selectedItem.isBusiness
+                )
+              }
+            >
+              View Listing Details
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => {
+                handleClickOpen(selectedItem.itemTitle, selectedItem.itemId);
+              }}
+            >
+              Disable Item
+            </Button>
+          </DialogActions>
         </Dialog>
 
         {/* Alert pop-ups for disabling and enabling*/}
